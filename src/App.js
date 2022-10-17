7224bdfc180423b6f10b90b8010bd4319456011e
Carlicho <carlosjlicho@gmail.com>
2022-10-17 18:02:48 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Image from "./components/Image";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const images = [
+    {   index: 1,
+        src: "https://picsum.photos/id/1/300/200"
+    }, 
+    {   index: 2,
+        src: "https://picsum.photos/id/10/300/200"
+    },
+    {   index: 3,
+        src: "https://picsum.photos/id/21/300/200", 
+    },
+    {   index: 4,
+        src: "https://picsum.photos/id/30/300/200", 
+    },
+    {   index: 5,
+        src: "https://picsum.photos/id/41/300/200", 
+    },
+    {   index: 6,
+        src: "https://picsum.photos/id/50/300/200",
+    },
+    {   index: 7,
+        src: "https://picsum.photos/id/61/300/200", 
+    },
+    {   index: 8,
+        src: "https://picsum.photos/id/70/300/200", 
+    },
+    {   index: 9,
+        src: "https://picsum.photos/id/81/300/200", 
+    },
+    {   index: 10,
+        src: "https://picsum.photos/id/90/300/200", 
+    },
+    {   index: 11,
+        src: "https://picsum.photos/id/101/300/200", 
+    },
+    {   index: 12,
+        src: "https://picsum.photos/id/110/300/200"
+    }
+];
+
+function App() {    
+    const imageList = images.filter(src => {
+        if (src.index % 2 === 0){
+            src.css = "text-danger"
+            return src
+        } else {
+            src.css = "text-warning"
+            return src
+        }
+
+    }).map(e => <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-3"><Image src={e.src} label={e.index} css={e.css}/></div>);
+
+    return (
+        <div className="App">
+            <h1>Lista de Imágenes</h1>
+            <h4 className="mb-3">Con "filter" muestra los números impares en <span className="text-warning">Amarillo</span> y los pares en <span className="text-danger">Rojo</span> </h4>
+            <div className="row w-100 text-center">
+                {imageList}
+            </div>
+        </div>
+    );
 }
 
 export default App;
